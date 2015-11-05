1d1bb4a04bc47995455e996486d204b97af51efb
Kailash <kaidurai@gmail.com>
2015-11-04 20:07:14 -0800

    had to redo migration because of schema conflict I think

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,15 @@ ActiveRecord::Schema.define(version: 20151105034704) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
-ActiveRecord::Schema.define(version: 20151101005733) do
+  create_table "decks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "prompt"
